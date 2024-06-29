4710f17b89d976a04904e8d566776dd20d90b70c
TharBuiui <itxaditya5@gmail.com>
2024-06-29 14:27:55 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -640,7 +640,7 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                                 InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                                 InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                             ],[
-                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/creatorrio")
+                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/aditya98A")
                                 ]
                             ]
                         )
