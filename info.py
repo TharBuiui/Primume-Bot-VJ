bac29e560e9ebd1a3167e38f2e8191dca20f4bea
TharBuiui <itxaditya5@gmail.com>
2024-06-29 14:30:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,7 +26,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
 PICS = (environ.get('PICS', 'https://graph.org/file/1f872af560c122cea5676.jpg')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/b69af2db776e4e85d21ec.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/How_To_Open_Linkl")
+MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/HowTo0penLink")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
@@ -69,7 +69,7 @@ OWNER_USERNAME = environ.get('OWNER_USERNAME', 'aditya98A') # owner username wit
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+fFmB_6zNOfIzMTdl')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/MovieHouseChannelbot')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/HowTo0penLink')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 't.me/+fFmB_6zNOfIzMTdl') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '+fFmB_6zNOfIzMTdl') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
